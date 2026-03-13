7a7062fa03296655fa09085ceb7dd44c61298d1a
apilot-dev <contact@apilot.net>
2026-03-13 20:59:57 +0000

    add curl product details example

---
+++ curl/product_details.sh
@@ -0,0 +1,4 @@
+curl --request GET \
+  --url "https://azone-real-time-amazon-data-api.p.rapidapi.com/product-details?asin=B0B8G1Q9DM&country=US" \
+  --header "X-RapidAPI-Key: YOUR_RAPIDAPI_KEY" \
+  --header "X-RapidAPI-Host: azone-real-time-amazon-data-api.p.rapidapi.com"
